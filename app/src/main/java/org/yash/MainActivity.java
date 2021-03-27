b106b82f56b66b53aa72dfec8929cf6310e3cbf5
Yash-Alchemy <yash@alchemytech.ca>
2021-03-27 21:05:19 +0530

    Spinner

--- app/src/main/java/org/yash/MainActivity.java
+++ app/src/main/java/org/yash/MainActivity.java
@@ -7,39 +7,45 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.switchmaterial.SwitchMaterial;
 
+import java.lang.reflect.Array;
+
 public class MainActivity extends AppCompatActivity{
 
-    Button button;
+    Spinner spinner;
+    String[] countries = {"India", "USA", "Japan", "Russia"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = findViewById(R.id.button);
-
-        LayoutInflater li = getLayoutInflater();
-        View layout = li.inflate(R.layout.custom_toast, (ViewGroup) findViewById(R.id.custom_toast_layout));
-
-
-        button.setOnClickListener(new View.OnClickListener() {
+        spinner = findViewById(R.id.country_spinner);
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, countries);
+        spinner.setAdapter(arrayAdapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onClick(View v) {
-                Toast toast = new Toast(getApplicationContext());
-                toast.setDuration(Toast.LENGTH_SHORT);
-                toast.setGravity(Gravity.CENTER_VERTICAL, 100, 0);
-                toast.setView(layout);
-                toast.show();
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, countries[position] , Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
             }
         });
+
     }
 }
