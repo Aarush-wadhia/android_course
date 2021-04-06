7dc300056d4cb1f3144668fcdd2259299388e3ed
aarushwadhia@gmail.com <yash@alchemytech.ca>
2021-04-06 20:15:40 +0530

    Implicit intent with handling permissions

--- app/src/main/java/org/yash/MainActivity.java
+++ app/src/main/java/org/yash/MainActivity.java
@@ -1,44 +1,99 @@
 package org.yash;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String name = "Yash";
+    private static final int CAMERA_REQUEST_CODE = 101;
+    private static final int CAMERA_PERMISSION_CODE = 201;
+    private static final int SETTINGS_REQUEST_CODE = 301;
+
+    ImageView imageView;
+    ConstraintLayout constraintLayout;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView textView = findViewById(R.id.textView);
+
         Button button = findViewById(R.id.button);
-
-        if (null != savedInstanceState) {
-            String value = savedInstanceState.getString("name");
-            if (null != value) {
-                textView.setText(value);
-            }
-        }
-
+        imageView = findViewById(R.id.imageView);
+        constraintLayout = findViewById(R.id.parent);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                textView.setText(name);
+                handlePermission();
             }
         });
     }
 
+    private void handlePermission() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            captureImage();
+        } else {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+                showSnackBar();
+            } else {
+                ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, CAMERA_PERMISSION_CODE);
+            }
+        }
+    }
+
+    private void showSnackBar() {
+        Snackbar.make(constraintLayout, "Grant the permission", Snackbar.LENGTH_INDEFINITE)
+                .setAction("Grant Permission", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                        intent.setData(Uri.parse("package:" + getPackageName()));
+                        startActivityForResult(intent, SETTINGS_REQUEST_CODE);
+                    }
+                }).show();
+    }
+
+    private void captureImage() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, CAMERA_REQUEST_CODE);
+    }
+
     @Override
-    protected void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putString("name", name);
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case CAMERA_REQUEST_CODE:
+
+                if (resultCode == RESULT_OK && null != data) {
+                    Bundle bundle = data.getExtras();
+                    if (null != bundle) {
+                        Bitmap b = (Bitmap) bundle.get("data");
+                        imageView.setImageBitmap(b);
+                    }
+                }
+                break;
+
+            case SETTINGS_REQUEST_CODE:
+                handlePermission();
+                break;
+        }
     }
 }
