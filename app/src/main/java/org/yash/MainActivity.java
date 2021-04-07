fe7ce902534e19abfb1c99f7e0fd773399ae7e6e
aarushwadhia@gmail.com <yash@alchemytech.ca>
2021-04-07 16:46:08 +0530

    Fragments

--- app/src/main/java/org/yash/MainActivity.java
+++ app/src/main/java/org/yash/MainActivity.java
@@ -11,6 +11,7 @@ import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,80 +21,26 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int CAMERA_REQUEST_CODE = 101;
-    private static final int CAMERA_PERMISSION_CODE = 201;
-    private static final int SETTINGS_REQUEST_CODE = 301;
 
-    ImageView imageView;
-    ConstraintLayout constraintLayout;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = findViewById(R.id.button);
-        imageView = findViewById(R.id.imageView);
-        constraintLayout = findViewById(R.id.parent);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                handlePermission();
-            }
-        });
-    }
+        Bundle bundle = new Bundle();
+        bundle.putString("name", "Yash");
+        Fragment fragment = new FirstFragment();
+        fragment.setArguments(bundle);
 
-    private void handlePermission() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
-            captureImage();
-        } else {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
-                showSnackBar();
-            } else {
-                ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, CAMERA_PERMISSION_CODE);
-            }
-        }
-    }
-
-    private void showSnackBar() {
-        Snackbar.make(constraintLayout, "Grant the permission", Snackbar.LENGTH_INDEFINITE)
-                .setAction("Grant Permission", new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-                        intent.setData(Uri.parse("package:" + getPackageName()));
-                        startActivityForResult(intent, SETTINGS_REQUEST_CODE);
-                    }
-                }).show();
-    }
-
-    private void captureImage() {
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(intent, CAMERA_REQUEST_CODE);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        switch (requestCode) {
-            case CAMERA_REQUEST_CODE:
-
-                if (resultCode == RESULT_OK && null != data) {
-                    Bundle bundle = data.getExtras();
-                    if (null != bundle) {
-                        Bitmap b = (Bitmap) bundle.get("data");
-                        imageView.setImageBitmap(b);
-                    }
-                }
-                break;
-
-            case SETTINGS_REQUEST_CODE:
-                handlePermission();
-                break;
-        }
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.container, fragment);
+        fragmentTransaction.commit();
     }
 }
