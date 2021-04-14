75a29ab81210d111f034164dbf83f04a5b9d3e0b
aarushwadhia@gmail.com <yash@alchemytech.ca>
2021-04-14 19:58:52 +0530

    Parsing JSON

--- app/src/main/java/org/yash/MainActivity.java
+++ app/src/main/java/org/yash/MainActivity.java
@@ -1,166 +1,64 @@
 package org.yash;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.util.Xml;
-import android.widget.LinearLayout;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserException;
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = MainActivity.class.getName();
-    Thread backgroundThread;
-    RecyclerView newsRecView;
-    String title = "", description = "", pubdate = "";
-    private ArrayList<News> newsArrayList = new ArrayList<>();
+    Button button;
+    TextView textView;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        newsRecView = findViewById(R.id.newsRecView);
-        getData();
-    }
-
-    private void getData(){
-        backgroundThread = new Thread(new Runnable() {
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.data);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                //doInBackground
-                InputStream inputStream = getInputStream();
-                if (null != inputStream) {
-                    try {
-                        initXMLParser(inputStream);
-                    } catch (XmlPullParserException | IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-                //onPost
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        NewsAdapter newsAdapter = new NewsAdapter(newsArrayList);
-
-                        newsRecView.setAdapter(newsAdapter);
-                        newsRecView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
-                    }
-                });
+            public void onClick(View v) {
+                getData();
             }
         });
-        backgroundThread.start();
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        backgroundThread.stop();
-    }
+    private void getData() {
+        Gson gson = new Gson();
+        String url = "https://jsonplaceholder.typicode.com/posts";
 
-    private void initXMLParser(InputStream inputStream) throws XmlPullParserException, IOException {
-        Log.d(TAG, "initXMLParser: Initializing XML Pull Parser");
-        XmlPullParser parser = Xml.newPullParser();
-        parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
-        parser.setInput(inputStream, null);
-        parser.next();
-
-        parser.require(XmlPullParser.START_TAG, null, "rss");
-        while (parser.next() != XmlPullParser.END_TAG) {
-            if (parser.getEventType() != XmlPullParser.START_TAG) {
-                continue;
-            }
-
-            parser.require(XmlPullParser.START_TAG, null, "channel");
-            while (parser.next() != XmlPullParser.END_TAG) {
-                if (parser.getEventType() != XmlPullParser.START_TAG) {
-                    continue;
-                }
-
-                if (parser.getName().equals("item")){
-                    parser.require(XmlPullParser.START_TAG, null, "item");
-
-                    while (parser.next() != XmlPullParser.END_TAG) {
-                        if (parser.getEventType() != XmlPullParser.START_TAG) {
-                            continue;
-                        }
-                        String tagName = parser.getName();
-                        switch (tagName) {
-                            case "title":
-                               title = getContent(parser, "title");
-                                break;
-
-                            case "description":
-                               description = getContent(parser, "description");
-                                break;
-
-                            case "pubDate":
-                               pubdate = getContent(parser, "pubDate");
-                                break;
-                            default:
-                                skipTag(parser);
-                                break;
-                        }
-                    }
-                    News news = new News(title, description, pubdate);
-                    newsArrayList.add(news);
-                } else {
-                    skipTag(parser);
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Post[] post = gson.fromJson(response, Post[].class);
+                for (Post p: post) {
+                    textView.setText(textView.getText() + "\n" + p.getTitle());
                 }
             }
-        }
-    }
-
-    private void skipTag(XmlPullParser parser) throws XmlPullParserException, IOException {
-        if (parser.getEventType() != XmlPullParser.START_TAG) {
-            throw  new IllegalArgumentException();
-        }
-
-        int num = 1;
-        while (num != 0) {
-            switch (parser.next()){
-                case XmlPullParser.START_TAG:
-                    num++;
-                    break;
-                case XmlPullParser.END_TAG:
-                    num--;
-                    break;
-                default:
-                    break;
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
             }
-        }
-    }
+        });
 
-    private String getContent(XmlPullParser parser, String tag) throws IOException, XmlPullParserException {
-        String content = "";
-        parser.require(XmlPullParser.START_TAG, null, tag); //description
-        if (parser.next() == XmlPullParser.TEXT) {
-            content = parser.getText();
-            parser.next();
-        }
-        return content;
-    }
-
-    private InputStream getInputStream() {
-        try {
-            URL url = new URL("https://timesofindia.indiatimes.com/rssfeeds/913168846.cms");
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-            return connection.getInputStream();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+        requestQueue.start();
     }
 }
